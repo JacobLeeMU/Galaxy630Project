2fd3a4bc96d91848b08a5d2bd2d75071a22233e3
Ellie White <elliewhite1420@gmail.com>
2026-05-02 15:14:11 +0000

    updated script and results

--- final_project.py
+++ final_project.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import time
 import torch
 import h5py
 import json
@@ -42,7 +43,7 @@ print(labels[0])
 
 ## display each image 
 
-num_iter = 5
+num_iter = 3
 
 f = open("results.txt", "w")
 
@@ -54,57 +55,53 @@ for i in range(len(images)):
 
     print(f'\n image {i+1}\n')
     f.write(f'image {i+1}\n')
+    
+    prompt = []
+    
+    prompt.append("Describe what you see in this image.\nASSISTANT: ")
+    prompt.append("Is the galaxy in this image viewed edge on or not?\nASSISTANT: ")
+    prompt.append("Is there a bar feature at the center of the galaxy?\nASSISTANT: ")
+    prompt.append("Does this galaxy have a spiral arm pattern?\nASSISTANT: ")
+    prompt.append("Is the central galaxy in this image more red or blue in color? What does this indicate about the galaxy's age and rate of star formation as a result? List the steps in your reasoning that led you to this conclusion.\nASSISTANT: ")
+    prompt.append("Please classify the galaxy in this image into one of the following: 'barred spiral', 'unbarred tight spiral', 'unbarred loose spiral', 'edge-on without bulge', or 'edge-on with bulge', and list the steps in your reasoning that led you to this conclusion.\nASSISTANT: ")
 
     response_list = []
     for j in range(num_iter):
         if j == 0:
             ## classify galaxy, also describe and explain galaxy
-            
-            prompt = []
-            
-            prompt.append("Describe what you see in this image.")
-            
-            prompt.append("Is the galaxy in this image viewed edge on or not?")
-            
-            #prompt.append("Does the galaxy appear to have a central bulge?")
-            
-            prompt.append("Is there a bar feature at the center of the galaxy?")
-            
-            prompt.append("Does this galaxy have a spiral arm pattern?")
-            
-            prompt.append("Is the central galaxy in this image more red or blue in color? What does this indicate about the galaxy's age and rate of star formation as a result? Please list the steps in your reasoning that led you to this conclusion.")
-
-            prompt.append("Please classify the galaxy in this image into one of the following: 'barred spiral', 'unbarred tight spiral', 'unbarred loose spiral', 'edge-on without bulge', or 'edge-on with bulge'. Please list the steps in your reasoning that led you to this conclusion.")
-
             responses = []
             for p in prompt:
-                question = "<image>\n"+p
+                question = "USER: <image>\n"+p
 
                 inputs = processor(text=question, images=image, return_tensors="pt").to(device)
                 input_len = inputs["input_ids"].shape[-1]
                 print("generating caption...")
-                output = model.generate(**inputs, max_new_tokens=500)
+                output = model.generate(**inputs, max_new_tokens=512)
                 response = processor.decode(output[0][input_len:], skip_special_tokens=True)
+                #response = processor.decode(output[0], skip_special_tokens=True)
                 responses.append(response)
 
                 # print output
                 print("question:", question)
                 print("model response:", response)
-            
+                time.sleep(1)
+
             response_list.append(responses)
 
         else:
             responses = []
             for k, p in enumerate(prompt):
-                revision_cue = "<image>\n"+f"Here is the previous prompt for the attached image: {p} \n the corresponding output was: {response_list[j-1][k]} \nCritique this output and provide an improved response if possible"
-                print("model response:", response)
+                prev_response = response_list[j-1][k]
+                revision_cue = "USER: <image>\n"+f"Here is the previous prompt for the attached image: {p}; the previous output was: {prev_response}. Please provide an improved version of the previous output, modified to improve accuracy and clarity\nASSISTANT: "
                 inputs = processor(text=revision_cue, images=image, return_tensors="pt").to(device)
                 input_len = inputs["input_ids"].shape[-1] #Gets the response length
                 print("generating revision...")
-                output = model.generate(**inputs, max_new_tokens=500)
+                output = model.generate(**inputs, max_new_tokens=512)
+                #response = processor.decode(output[0], skip_special_tokens=True)
                 response = processor.decode(output[0][input_len:], skip_special_tokens=True) #Slices off everything before input_len
                 print("model response:", response)
                 responses.append(response)
+                time.sleep(1) 
             response_list.append(responses)
 
     for i,q in enumerate(prompt):
@@ -112,4 +109,4 @@ for i in range(len(images)):
         for j,r in enumerate([row[i] for row in response_list]):
             f.write(f"Revision {j}: {r}\n")
         f.write("\n\n")
-
+f.close()
